5cbf1e5a357cbc7d13f5d6b5c78d15eb49de1e4c
Ahrav Dutta <ahravdutta02@gmail.com>
2019-07-28 19:42:14 -0700

    find vowel

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  var count = 0;
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) count++;
+  }
+  return count;
+}
+
+console.log(vowels('ahrav'));
 
 module.exports = vowels;
